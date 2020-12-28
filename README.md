07d45e2dcda97476138f50f3272a931348aea82d
bybogon <qhrhs59@gmail.com>
2020-12-28 22:51:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+Automatic Lottery Project with puppeteer
+
+Nodejs + Puppeteer + Typescript
+
+Goal
+
+1. Make a crawler with puppeteer
+2. Set a scheduler to run it weekly
+3. Send me a notification or an email if there's no money left to do a lottery
+4. Gather all numbers that was first prize
+5. Save them in a database and try to predict(guess) future first prize number by those data
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import puppeteer = require("puppeteer");
+
+const LOTTERY_URL: string =
+  "https://www.dhlottery.co.kr/user.do?method=login&returnUrl=";
+const userId:string = "test";
+const password:string = "test";
+const passwordType:string = 'input[type="password"]';
+
+const iframeUrl:string = "https://ol.dhlottery.co.kr/olotto/game/game645.do";
+
+(async () => {
+  const limitedArrLength:number = 5;
+  let chosenNumbers = [];
+  const browser = await puppeteer.launch({ headless: false }); // default is true
+  const [page] = await browser.pages();
+  await page.goto(LOTTERY_URL, { waitUntil: "networkidle2" });
+  await page.type('input[id="userId"]', userId);
+  await page.type(passwordType, password);
+  await page.click('a[class="btn_common lrg blu"]');
+  const popup:Promise<puppeteer.Page> = new Promise(resolve =>
+    browser.on("targetcreated", target => resolve(target.page()))
+  );
+  const newPage = await popup;
+  await newPage.close();
+  const pages = await browser.pages();
+
+  console.log(pages.length);
+
+  //   await page.hover('li[class="gnb1"]');
+  //   await page.click('a[href="javascript:goLottoBuy(2);"', { delay: 3000 });
+
+  await page.goto(iframeUrl);
+  //   const lotteryFrame = page
+  //     .frames()
+  //     .find(frame => frame.name().includes("ifrm_tab"));
+  //   console.log(lotteryFrame);
+  //   await page.waitForSelector("iframe");
+
+  //   const elementHandle = await page.$("iframe #ifrm_tab");
+  //   const frame = await elementHandle.contentFrame();
+  //   await frame.waitForSelector('[href="#divWay2Buy3"]');
+  await page.click('a[href="#divWay2Buy3"]', { delay: 1000 });
+  const fullContent = await page.content();
+  console.log(fullContent);
+
+  const firstPrize = fullContent
+    .split("<li>")
+    .find(c => c.includes("1등 예상 총 당첨금액"));
+  console.log(firstPrize);
+  const myList:any = await page.$("#myList");
+  console.log(myList);
+  const myListQ = await page.queryObjects(myList);
+  console.log(myListQ);
+  //   const myListLength = await page.evaluate(body => body.innerHTML, myList);
+  //   console.log(myListLength);
+  await myList.dispose();
+
+  const forLoop = async () => {
+    for (let i = 1; i < limitedArrLength; i++) {
+      const element = await page.waitForSelector(
+        `#myList > li:nth-child(${i}) > input[type=checkbox]`,
+        { visible: true }
+      );
+      // console.log(element);
+
+      await element.click();
+    }
+  };
+  forLoop();
+  // await page.waitForSelector("li[gnb1]", { visible: true });
+
+  //   const context = await page.content();
+  //   console.log(context);
+
+  //   await browser.close();
+})();
